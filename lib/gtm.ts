21b393811b48a4f6863471e8484f708dbcbfb711
Jay McMullen <j@1f.io>
2021-08-30 14:39:19 +1000

    fix error

--- lib/gtm.ts
+++ lib/gtm.ts
@@ -1,10 +1,13 @@
+const w = window as any
+
 export const trackPageView = (url: string) => {
-  window &&
-    (window as any).gtag('config', process.env.NEXT_PUBLIC_GOOGLE_TAG_ID, {
+  w &&
+    w.gtag &&
+    w.gtag('config', process.env.NEXT_PUBLIC_GOOGLE_TAG_ID, {
       page_path: url,
     })
 }
 
 export const trackEvent = (action: string, params: any) => {
-  window && (window as any).gtag('event', action, params)
+  w && w.gtag && w.gtag('event', action, params)
 }
